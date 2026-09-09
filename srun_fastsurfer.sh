56e8d50acdacf9cdcad69b4a6bd765cc6ff3de58
David Kügler <david.kuegler@dzne.de>
2026-09-09 18:58:01 +0200

    Address GitHub Copilot review comments
    Signed-off-by: David Kügler <david.kuegler@dzne.de>

--- srun_fastsurfer.sh
+++ srun_fastsurfer.sh
@@ -159,8 +159,8 @@ SLURM-related options:
 --extra_slurm_options <sbatch options>,
 --extra_slurm_options_seg <sbatch options>, and
 --extra_slurm_options_surf <sbatch options>: Extra options passed to the sbatch call, needs to be double quoted
-  to allow quoted strings, e.g. --extra_slurm_options_seg "--reservation=my_res --qos=high". Options are split at
-  whitespace, so avoid white spaces! Does not affect cleanup and copy jobs.
+  for multiple arguments, e.g. --extra_slurm_options_seg "--reservation=my_res --qos=high". Option does not support
+  nested quotes and are split at whitespace to separate different parameters! Does not affect cleanup and copy jobs.
 --time_seg <timelimit>, and
 --time_surf <timelimit>: a per-image time limit for individual the segmentation and surface reconstruction steps,
   respectively. <timelimit> must be a number in minutes, default seg: ${timelimit_seg}min, surf: ${timelimit_surf}min.
@@ -598,10 +598,12 @@ then
   seg_slurm_sched=("--mem=${mem_seg}G" "--cpus-per-task=$num_cpus_per_task" -J "FastSurfer-Seg-$USER"
                    --time=$((timelimit_seg * real_num_cases_per_task + 5))
                    "${slurm_partition[@]}" "${slurm_email[@]}" "${jobarray_option[@]}"
-                   -o "$hpc_work/logs/seg_%A_%a.log" "${slurm_extra_seg[@]}" "$seg_cmd_filename")
+                   -o "$hpc_work/logs/seg_%A_%a.log")
   if [[ "$cpu_only" == "true" ]] ; then debug "Schedule SLURM job without gpu"
   else seg_slurm_sched+=(--gpus-per-task=1)
   fi
+  # append slurm_extra_seg arguments after fixed one gpu per task, then the script to execute
+  seg_slurm_sched+=("${slurm_extra_seg[@]}" "$seg_cmd_filename")
   log "chmod +x $seg_cmd_filename"
   chmod +x "$seg_cmd_file"
   log "sbatch --parsable ${seg_slurm_sched[*]}"
